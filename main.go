0c316ec47aad893b430ed123dd85bc891508d096
Oscar I Hernandez <oscarihernandez@Oscars-Personal-MacBook-Pro.local>
2020-09-06 18:16:49 -0500

    past lesson 31

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+var y = 100 + 32 //declare with var, assign with =
+var z int
+var blah string = "shaken, not stirred"
+
 func main() {
 	fmt.Println("Hello World!")
 	foo()
@@ -10,8 +14,22 @@ func main() {
 		if i%2 == 0 {
 			fmt.Println(i)
 		}
-
 	}
+	n, err := fmt.Println("String", 44, false)
+	fmt.Println(n)
+	fmt.Println(err)
+
+	x := 42 //declare and assign
+	fmt.Println(x)
+	x = 5 //assign (already declared)
+	fmt.Println(x)
+
+	fmt.Println(y)
+	fmt.Println("z =", z)
+	fmt.Printf("%T\n", z)
+	fmt.Println("blah =", blah)
+	fmt.Printf("%T\n", blah)
+
 }
 
 func foo() {
